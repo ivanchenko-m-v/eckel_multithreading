12bb95add795209698660546fef761a6643bb95e
Michael Ivanchenko <misha@kvvaiu.ru>
2020-03-15 23:35:35 +0300

    2020-03-15 first commit

---
+++ task01/src/main/java/ru/imv/Task01App.java
@@ -0,0 +1,13 @@
+package ru.imv;
+
+/**
+ * Hello world!
+ *
+ */
+public class Task01App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ task01/src/test/java/ru/imv/Task01AppTest.java
@@ -0,0 +1,20 @@
+package ru.imv;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Task01AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
