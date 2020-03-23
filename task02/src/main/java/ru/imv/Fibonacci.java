b7c9a5d53a9e4e54eb51419327666c34cdd51719
M.Ivanchenko <bearny@ya.ru>
2020-03-23 20:54:17 +0300

    2020-03-23 Fibonacci task02 complete

--- task02/src/main/java/ru/imv/Fibonacci.java
+++ task02/src/main/java/ru/imv/Fibonacci.java
@@ -3,42 +3,35 @@ package ru.imv;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Fibonacci
-{
+public class Fibonacci {
     private static Map<Integer, Integer> fibonacciNumbers = new TreeMap<>();
-    private final  Integer               N;
+    private final Integer N;
 
-    static
-    {
-        fibonacciNumbers.put( 0, 0 );
-        fibonacciNumbers.put( 1, 1 );
+    static {
+        fibonacciNumbers.put(0, 0);
+        fibonacciNumbers.put(1, 1);
     }
 
-    public Fibonacci( Integer number )
-    {
-        if( number < 0 )
-        {
-            throw new IndexOutOfBoundsException( "Invalid N in Fibonacci row: N=" + number );
+    public Fibonacci(Integer number) {
+        if (number < 0) {
+            throw new IndexOutOfBoundsException("Invalid N in Fibonacci row: N=" + number);
         }
-        if( number > 1 )
-        {
-            fibonacciNumbers.computeIfAbsent( number, unused -> computeFibonacci( number ) );
+        if (number > 1) {
+            fibonacciNumbers.computeIfAbsent(number, unused -> computeFibonacci(number));
         }
         N = number;
     }
 
-    private Integer computeFibonacci( Integer number )
-    {
-        Integer n_1 = fibonacciNumbers.computeIfAbsent( number - 1, unused -> computeFibonacci( number - 1 ) );
-        Integer n_2 = fibonacciNumbers.computeIfAbsent( number - 2, unused -> computeFibonacci( number - 2 ) );
+    private Integer computeFibonacci(Integer number) {
+        Integer n_1 = fibonacciNumbers.computeIfAbsent(number - 1, unused -> computeFibonacci(number - 1));
+        Integer n_2 = fibonacciNumbers.computeIfAbsent(number - 2, unused -> computeFibonacci(number - 2));
 
         return n_1 + n_2;
     }
 
-    public List<Integer> getValues( )
-    {
-        return fibonacciNumbers.keySet( ).stream( )
-                               .map( key -> fibonacciNumbers.get( key ) )
-                               .collect( Collectors.toList( ) );
+    public List<Integer> getValues() {
+        return fibonacciNumbers.keySet().stream()
+                .map(key -> fibonacciNumbers.get(key))
+                .collect(Collectors.toList());
     }
 }
--- task02/src/main/java/ru/imv/Task02App.java
+++ task02/src/main/java/ru/imv/Task02App.java
@@ -1,13 +1,20 @@
 package ru.imv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class Task02App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Task02App {
+    public static void main(String[] args) {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 5; i < 25; i += 5) {
+            Thread t = new Thread(new FibonacciProducer(new Fibonacci(i)));
+            t.setName("Thread " + String.format("%02d", i/5));
+            threads.add(t);
+        }
+        threads.forEach(Thread::start);
+        System.out.println("Waiting....");
     }
 }
